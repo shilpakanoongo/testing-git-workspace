6dd6514584df1c53571bbbe957ee3631d47035db
Shilpa Kanoongo <52091108+shilpakanoongo@users.noreply.github.com>
2024-04-01 22:17:34 -0400

    Update Demo1.java
    add m2 () method

--- TB24_TestingDemo/src/com/cjc/Demo1.java
+++ TB24_TestingDemo/src/com/cjc/Demo1.java
@@ -5,6 +5,10 @@ public class Demo1 {
 	{
 		System.out.println("This is m1() method");
 	}
+       public void m2()
+	{
+		System.out.println("This is m2() method");
+	}
 
 	public static void main(String[] args) {
 		System.out.println("My first GitHub Program");
